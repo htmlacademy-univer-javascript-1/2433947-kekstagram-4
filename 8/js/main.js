78f228cb2e2482fb0fff56c3c86a57dd241ea145
Keks <keks@htmlacademy.ru>
2023-11-30 19:31:41 +0300

    :heavy_check_mark: Сборка #8

---
+++ 8/js/main.js
@@ -0,0 +1,10 @@
+import {getPhotoDescriptions} from './photo-descriptions.js';
+import {creatMiniatures} from './draw-miniatures.js';
+import {renderBigPicture} from './big-picture-modal.js';
+
+const picturesData = getPhotoDescriptions();
+
+creatMiniatures(picturesData);
+renderBigPicture();
+
+export {picturesData};
